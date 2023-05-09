9b37f82dc8c431f2f6a79b647b13f7d403c72bfe
Mofeng <363034937@qq.com>
2023-05-09 20:20:22 +0800

    fix(types): fix `compose`

--- types/test/transform.ts
+++ types/test/transform.ts
@@ -1,15 +1,25 @@
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
-import { pipe } from '../transform'
+import { compose, pipe } from '../transform'
 
 // ========== pipe and compose==========
 
 import { max, add, mul } from '../math'
 
-const case1 = pipe(max, add, mul)
+const case0_0 = pipe()
+const case0_1 = compose()
+type Expected0 = () => void
+
+const case1_0 = pipe(max, add, mul)
+const case1_1 = compose(mul, add, max)
 type Expected1 = (arr: number[]) => number
 
 type cases = [
-  Expect<Equal<typeof case1, Expected1>>,
+  Expect<Equal<typeof case0_0, Expected0>>,
+  Expect<Equal<typeof case0_1, Expected0>>,
+
+  Expect<Equal<typeof case1_0, Expected1>>,
+  Expect<Equal<typeof case1_1, Expected1>>,
+
 ]
--- types/transform.d.ts
+++ types/transform.d.ts
@@ -23,33 +23,38 @@ export const pipe: <Funcs extends Fn[]>(
   ...funcs:
     Pipeable<Funcs> extends Funcs
     ? Funcs                 // the given funcs is okay to use.
-    : Pipeable<Funcs>  // use this to get the correct type hints.
+    : Pipeable<Funcs>       // use this to get the correct type hints.
 ) =>
   Funcs extends [] ? () => void :
   Funcs extends [infer First extends Fn, ...infer _, infer Last extends Fn] ? (...args: Parameters<First>) => ReturnType<Last> :
   Funcs extends [infer First] ? First :
   never;
 
-/**
- * Reverse the given tuple.
- */
-type Reverse<T extends any[], R extends any[] = []> =
-  T extends [infer First, ...infer Remaining]
-  ? Reverse<Remaining, [First, ...R]>
-  : R
+// the reverse version of `Pipeable`
+type Composeable<Funcs extends Fn[], Prev = () => any> =
+  Funcs extends [...infer Remaining extends Fn[], infer Last]
+  ? [
+    ...Pipeable<Remaining, Last>,
+    Prev extends (...args: any[]) => infer R ?
+    Last extends (...args: infer A) => any ?
+    A[0] extends R ? Last
+    : (arg: R) => any
+    : (arg: R) => any
+    : never
+  ]
+  : [];
 
 export const compose: <Funcs extends Fn[]>(
   ...funcs:
-    Pipeable<Reverse<Funcs>> extends Reverse<Funcs>
-    ? Reverse<Funcs>                 // the given funcs is okay to use.
-    : Pipeable<Reverse<Funcs>>  // use this to get the correct type hints.
+    Composeable<Funcs> extends Funcs
+    ? Funcs                 // the given funcs is okay to use.
+    : Composeable<Funcs>  // use this to get the correct type hints.
 ) =>
-  Reverse<Funcs> extends [] ? () => void :
-  Reverse<Funcs> extends [infer First extends Fn, ...infer _, infer Last extends Fn] ? (...args: Parameters<First>) => ReturnType<Last> :
-  Reverse<Funcs> extends [infer First] ? First :
+  Funcs extends [] ? () => void :
+  Funcs extends [infer First extends Fn, ...infer _, infer Last extends Fn] ? (...args: Parameters<Last>) => ReturnType<First> :
+  Funcs extends [infer Last] ? Last :
   never;
 
 export const call: CurryWarpper<
   <Func extends Fn>(fun: (...args: Parameters<Func>) => ReturnType<Func>, args: Parameters<Func>) => any
 >;
-
