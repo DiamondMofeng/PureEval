8bfa8e2db5ce650c37cb992ecfb628fa2181121a
Mofeng <363034937@qq.com>
2023-05-09 19:29:35 +0800

    feat(types): add `transform.compose`

--- types/transform.d.ts
+++ types/transform.d.ts
@@ -5,11 +5,7 @@ import type { CurryWarpper } from "./curry";
 */
 type Fn = (...args: any[]) => any
 
-export const call: CurryWarpper<
-  <Fn extends (...args: any[]) => any>(fun: (...args: Parameters<Fn>) => ReturnType<Fn>, args: Parameters<Fn>) => any
->;
-
-type PipeableArray<Funcs extends Fn[], Prev = () => any> =
+type Pipeable<Funcs extends Fn[], Prev = () => any> =
   Funcs extends [infer First, ...infer Remaining extends Fn[]]
   ? [
     Prev extends (...args: any[]) => infer R ?
@@ -19,20 +15,41 @@ type PipeableArray<Funcs extends Fn[], Prev = () => any> =
     : (arg: R) => any
     : never
     ,
-    ...PipeableArray<Remaining, First>
+    ...Pipeable<Remaining, First>
   ]
   : [];
 
 export const pipe: <Funcs extends Fn[]>(
   ...funcs:
-    PipeableArray<Funcs> extends Funcs
+    Pipeable<Funcs> extends Funcs
     ? Funcs                 // the given funcs is okay to use.
-    : PipeableArray<Funcs>  // use this to get the correct type hints.
+    : Pipeable<Funcs>  // use this to get the correct type hints.
 ) =>
   Funcs extends [] ? () => void :
   Funcs extends [infer First extends Fn, ...infer _, infer Last extends Fn] ? (...args: Parameters<First>) => ReturnType<Last> :
   Funcs extends [infer First] ? First :
-  never
+  never;
 
+/**
+ * Reverse the given tuple.
+ */
+type Reverse<T extends any[], R extends any[] = []> =
+  T extends [infer First, ...infer Remaining]
+  ? Reverse<Remaining, [First, ...R]>
+  : R
 
+export const compose: <Funcs extends Fn[]>(
+  ...funcs:
+    Pipeable<Reverse<Funcs>> extends Reverse<Funcs>
+    ? Reverse<Funcs>                 // the given funcs is okay to use.
+    : Pipeable<Reverse<Funcs>>  // use this to get the correct type hints.
+) =>
+  Reverse<Funcs> extends [] ? () => void :
+  Reverse<Funcs> extends [infer First extends Fn, ...infer _, infer Last extends Fn] ? (...args: Parameters<First>) => ReturnType<Last> :
+  Reverse<Funcs> extends [infer First] ? First :
+  never;
+
+export const call: CurryWarpper<
+  <Func extends Fn>(fun: (...args: Parameters<Func>) => ReturnType<Func>, args: Parameters<Func>) => any
+>;
 
