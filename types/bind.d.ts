464d3a9ee73fa1ae09041f6c1077bf42c45a9bd9
Mofeng <363034937@qq.com>
2023-05-11 11:51:32 +0800

    perf(types): remove unnecessary `Result` params

--- types/bind.d.ts
+++ types/bind.d.ts
@@ -8,19 +8,19 @@ export const _: { _P: 1 };
 
 type $_ = typeof _;
 
-type ProcessParams<OriParams extends any[], Params extends any[], Result extends unknown[] = []> =
+type ProcessParams<OriParams extends any[], Params extends any[]> =
   Params extends [...infer Remaining, infer Last]
   ? Last extends $_
-  ? ProcessParams<OriParams, Remaining, [[0, ...OriParams][Params['length']], ...Result]> // `0` is a placeholder for index.
-  : ProcessParams<OriParams, Remaining, Result>
-  : Result
+  ? [...ProcessParams<OriParams, Remaining>, [0, ...OriParams][Params['length']]] // `0` is a placeholder for index.
+  : ProcessParams<OriParams, Remaining>
+  : []
 
-type PlaceHoldAbleParams<Params extends any[], Result extends unknown[] = []> =
+type PlaceholdAbleParams<Params extends any[]> =
   Params extends [infer First, ...infer Remaining]
-  ? [...Result, First | $_, ...PlaceHoldAbleParams<Remaining>]
-  : Result
+  ? [First | $_, ...PlaceholdAbleParams<Remaining>]
+  : []
 
-export const bind: <F extends Fn, P extends PlaceHoldAbleParams<Parameters<F>>>(
+export const bind: <F extends Fn, P extends PlaceholdAbleParams<Parameters<F>>>(
   func: F,
   ...args: P
 ) => (...args: ProcessParams<Parameters<F>, P>) => ReturnType<F>
