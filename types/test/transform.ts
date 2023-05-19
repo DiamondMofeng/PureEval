97774568af55f31acc1aacade44763fd4422be93
Mofeng <363034937@qq.com>
2023-05-19 22:57:04 +0800

    tests(transform): add a critical test case

--- types/test/transform.ts
+++ types/test/transform.ts
@@ -15,6 +15,15 @@ const case1_0 = pipe(max, add(1), mul(2))
 const case1_1 = compose(mul(1), add(1), max)
 type Expected1 = (arr: number[]) => number
 
+// TODO fix this
+//      see https://github.com/microsoft/TypeScript/issues/54223
+//      Ts have issue when infering parameters from overloaded function.
+//      So current solution have some limitations.
+
+/// @ts-expect-error
+const case2_0 = pipe(max, add, mul)
+/// @ts-expect-error
+const case2_1 = compose(mul, add, max)
 
 type Parent = { name: string }
 type Child = { name: string, age: number }
@@ -22,13 +31,13 @@ type Child = { name: string, age: number }
 const getChildAge = (child: Child) => child.age
 const genParent = (): Parent => ({ name: 'foo' })
 
-const case2_0 = pipe(
+const case3_0 = pipe(
   genParent,
   ///@ts-expect-error
   getChildAge,
 )
 
-const case2_1 = compose(
+const case3_1 = compose(
   ///@ts-expect-error
   getChildAge,
   genParent,
